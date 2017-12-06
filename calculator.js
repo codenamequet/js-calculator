18ce40e70719ee2c6089eeab00a63f5a2b6dabe8
Your Name <you@example.com>
2017-12-06 18:45:40 -0500

    added basic math functions

--- calculator.js
+++ calculator.js
@@ -1,2 +1,21 @@
 var calculator = {
 }
+
+let num1 = ''
+let num2 = ''
+
+function add(num1, num2) {
+    num1 + num2
+}
+
+function subtract(num1, num2) {
+    num1 - num2
+}
+
+function multiply (num1, num2) {
+    num1 * num2
+}
+
+function divide (num1, num2) {
+    num1 / num2
+}
