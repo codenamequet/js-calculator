beb45481c06aab2dde8e24c2cd913f9650e55ed6
Your Name <you@example.com>
2017-12-07 15:32:17 -0500

    MVP

--- calculator.js
+++ calculator.js
@@ -18,5 +18,13 @@ divide: function (num1, num2) {
 
 square: function (num1) {
     return num1 * num1
+},
+
+sqrt: function (num1) {
+    return Math.sqrt(num1)
+},
+
+tip: function (num1) {
+    return num1 * .2
 }
 }
